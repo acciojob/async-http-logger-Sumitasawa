c2272a6e8b78f6735a701b5063aba2cb461f0d37
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-12-23 13:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+async function getData(){
+	try{
+	const res=await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	if(!res.ok){
+		throw new Error("Network response was not ok")
+	}
+	const data=await res.json();
+	console.log(data);
+	}catch(err){
+		console.log(err)
+	}
+}
+getData();
